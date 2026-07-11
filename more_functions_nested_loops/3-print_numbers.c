4f859b80c6dba7c457f1bca7a6285440f8c47946
SultanJafari <Sultan06668@gmail.com>
2026-07-11 15:24:17 +0000

    Add print_numbers function

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ * print_numbers - Prints the numbers from 0 to 9,
+ * followed by a new line.
  *
  * Return: void
  */
@@ -11,9 +12,7 @@ void print_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		/* المرة الأولى: نستخدم _putchar لطباعة الرقم */
 		_putchar(i + '0');
 	}
-	/* المرة الثانية: نستخدم _putchar لطباعة السطر الجديد */
 	_putchar('\n');
 }
