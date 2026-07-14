cf4258f98631d133d5ab5d42e978fcf4f6e5eee2
SultanJafari <Sultan06668@gmail.com>
2026-07-14 18:10:44 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _strlen - returns the length of a string
+ * @s: the pointer to the string
  *
- * Return: Always 0.
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-    char *str;
-    int len;
+	int length = 0;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
