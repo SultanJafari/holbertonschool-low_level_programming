e0ba875d280644737ed94cb2c4b3f6234f553215
SultanJafari <Sultan06668@gmail.com>
2026-07-19 11:30:54 +0000

    Add malloc_free directory

--- malloc_free/_putchar.c
+++
@@ -1,8 +0,0 @@
-cat > _putchar.c <<EOF
-#include <unistd.h>
-
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-EOF
--- malloc_free/main.h
+++
@@ -1,9 +0,0 @@
-cat > main.h <<EOF
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-
-#endif
-EOF
