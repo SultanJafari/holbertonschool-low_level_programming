bfbd70f6b31044cf1085aaff9b0d17a22c75e77e
SultanJafari <Sultan06668@gmail.com>
2026-07-27 16:35:03 +0000

    void array_iterator(int *array, size_t size, void (*action)(int));

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -8,5 +8,5 @@ int _putchar(char c);
 
 /* Task 0: Print name */
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* FUNCTION_POINTERS_H */
