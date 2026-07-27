5fe013ebeb54a86c7bb094ea73c82950845fdbbb
SultanJafari <Sultan06668@gmail.com>
2026-07-27 16:42:54 +0000

    int int_index(int *array, int size, int (*cmp)(int))

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,11 +3,9 @@
 
 #include <stddef.h>
 
-/* Prototype for _putchar if needed */
 int _putchar(char c);
-
-/* Task 0: Print name */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int)):
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* FUNCTION_POINTERS_H */
