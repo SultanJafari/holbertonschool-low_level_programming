d4010fea3d9f413f4b9cf23dca2fddce61018b23
SultanJafari <Sultan06668@gmail.com>
2026-08-11 13:34:03 +0000

    Task1

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,46 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * main - check the code
+ * add_dnodeint - adds a new node at the beginning of a doubly linked list
+ * @head: pointer to the head of the list
+ * @n: integer to store in the new node
  *
- * Return: Always EXIT_SUCCESS.
+ * Return: address of the new node, or NULL if it fails
  */
-int main(void)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *head;
+	dlistint_t *new;
 
-    head = NULL;
-    add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
-    print_dlistint(head);
-    return (EXIT_SUCCESS);
+	/* Allocate memory for the new node. */
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	/* Store the given integer in the new node. */
+	new->n = n;
+
+	/*
+	 * The new node becomes the first node,
+	 * so it has no previous node.
+	 */
+	new->prev = NULL;
+
+	/*
+	 * The old head becomes the next node
+	 * after the new node.
+	 */
+	new->next = *head;
+
+	/*
+	 * If the list is not empty, update the old head
+	 * so its previous pointer points to the new node.
+	 */
+	if (*head != NULL)
+		(*head)->prev = new;
+
+	/* Update the head to point to the new node. */
+	*head = new;
+
+	return (new);
 }
