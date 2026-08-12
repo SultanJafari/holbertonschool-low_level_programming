ad5f60222cbb4c9a2c2635b72f5fa4a71690b326
Sultanjafari <Sultan06668@gmail.com>
2026-08-12 16:38:54 +0000

    t0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,31 +12,24 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int i;
 
-	/* Return NULL if size is 0 */
 	if (size == 0)
 		return (NULL);
 
-	/* Allocate memory for the hash table structure */
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
 
-	/* Allocate memory for the array of hash_node_t pointers */
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
-		/* Free the previously allocated hash table to prevent memory leaks */
 		free(ht);
 		return (NULL);
 	}
 
-	/* Initialize each element of the array to NULL */
 	for (i = 0; i < size; i++)
-	{
 		ht->array[i] = NULL;
-	}
 
 	return (ht);
 }
