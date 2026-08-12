c40aac048735d22763624f29586dc2bd88c0ea2c
Sultanjafari <Sultan06668@gmail.com>
2026-08-12 17:17:33 +0000

    t3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || key[0] == '\0')
+	return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+	{
+		new_node = malloc(sizeof(hash_node_t));
+
+               if (new_node == NULL)
+	return (0);
+
+	       new_node->key = strdup(key);
+		new_node->value = strdup(value);
+	}
+	if (new_node->key == NULL || new_node->value == NULL)
+{
+	free(new_node->key);
+	free(new_node->value);
+	free(new_node);
+	return (0);
+}
+
+}
