210d198193f32acad0ae9fa72ddba93659a60fae
SultanJafari <Sultan06668@gmail.com>
2026-08-19 13:54:56 +0000

    T1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,13 +21,6 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	/* 
-	 * Open with:
-	 * - O_CREAT: create the file if it does not exist
-	 * - O_WRONLY: open for writing only
-	 * - O_TRUNC: truncate the file to 0 length if it already exists
-	 * Permissions: 0600 (read and write for owner only, as required by ALX)
-	 */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
